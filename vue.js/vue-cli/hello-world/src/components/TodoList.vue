1338e192d3ac79500292146d5335ebd1c7fd00d7
Mandy997 <1215722711@qq.com>
2018-08-01 09:12:12 +0800

    添加VUE-CLI项目练习

---
+++ vue.js/vue-cli/hello-world/src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>
+            <input type="text" v-model="inputValue"/>
+            <button @click="handleSubmit">提交</button>
+        </div>
+        <ul>
+           
+        </ul>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            inputValue:'',
+            list:[]
+        }
+    },
+    methods:{
+        handleSubmit(){
+            this.list.push(this.inputValue);
+            this.inputValue='';
+        }
+    }
+}
+</script>
+
+<style>
+</style>
