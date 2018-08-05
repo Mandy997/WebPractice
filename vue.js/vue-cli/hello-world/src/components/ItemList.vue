8451c0c737d680902028cda112c1578f6b7d6b05
Mandy997 <1215722711@qq.com>
2018-08-05 11:40:32 +0800

    vue的声明周期钩子与计算属性

---
+++ vue.js/vue-cli/hello-world/src/components/ItemList.vue
@@ -0,0 +1,17 @@
+<template>
+    <li @click="handleClick">{{content}}</li>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {};
+  },
+  props: ["content", "index"],
+  methods: {
+    handleClick: function(index) {
+      this.$emit("delete", index);
+    }
+  }
+};
+</script>
--- vue.js/vue-cli/hello-world/src/components/TodoList.vue
+++ vue.js/vue-cli/hello-world/src/components/TodoList.vue
@@ -4,9 +4,7 @@
             <input type="text" v-model="inputValue"/>
             <button @click="handleSubmit">提交</button>
         </div>
-        <ul>
-           
-        </ul>
+         <todo-item v-for="(item,index) of list" :key="index" :content="item" :index="index" @delete="hanleDelete"></todo-item>
     </div>
 </template>
 
