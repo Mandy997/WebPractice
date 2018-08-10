17dbbacb528a22bdca6e0c70b12e3e26272de097
Mandy997 <1215722711@qq.com>
2018-08-10 08:10:55 +0800

    刘红红挺傻的

--- vue.js/vue-cli/travel/src/main.js
+++ vue.js/vue-cli/travel/src/main.js
@@ -11,8 +11,9 @@ import './assets/style/border.css'
 
 // 移动端300ms 点击事件延迟解决方案，使用fastclick 库
 import fastClick from 'fastclick'
-
+// 与模块系统一起使用时，必须通过Vue.use（）显式安装路由器
 Vue.use(VueRouter);
+
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
 
