e5b858ad608ca995186e90cfe145801b4bba8418
Mandy997 <chuntian997@gmail.com>
2019-06-10 22:53:03 +0800

    初始化es6

--- vue.js/vue-cli/travel/vue.config.js
+++ vue.js/vue-cli/travel/vue.config.js
@@ -50,7 +50,13 @@ module.exports = {
         port: 8080,
         https: false,
         hotOnly: false,
-        proxy: null, // 设置代理
+        proxy: {
+            '/api': {
+                target: 'localhost:4000',
+                ws: true,
+                changeOrigin: true
+            }
+        }, // 设置代理
         before: app => {}
     },
     // 第三方插件配置
